180d9a4f45093de1303376b2b90dfb0e49fe01f6
mahmoud fouad <mfapolice@gmail.com>
2022-08-16 05:24:45 +0200

    done form

--- src/component/meetups/NewMeetup.js
+++ src/component/meetups/NewMeetup.js
@@ -9,6 +9,21 @@ function NewMeetup() {
           <label htmlFor="title">Meetup Title</label>
           <input type="text" required id="title" />
         </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Meetup Address</label>
+          <input type="text" required id="address" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Meetup Description</label>
+          <textarea rows="5" required id="description" />
+        </div>
+        <div>
+          <button className={classes.action}>add Meetup</button>
+        </div>
       </form>
     </Card>
   );
