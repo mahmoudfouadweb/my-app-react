e0d6c40e826a9f8ab5c539b36d37a7a412cdde22
mahmoud fouad <mfapolice@gmail.com>
2022-08-16 02:30:07 +0200

    layout.js and Card

--- src/component/meetups/meetupitem.js
+++ src/component/meetups/meetupitem.js
@@ -1,21 +1,18 @@
-import classes from './MeetupList.module.css';
+import classes from './MeetupItem.module.css';
 
 function MeetupItem(props) {
   return (
     <li key={props.id} className={classes.item}>
-      <div>
-        <img
-          className={classes.image}
-          src="{props.image}"
-          alt="{props.title}"
-        />
+      <div className={classes.image}>
+        <img src={props.image} alt={props.title} />
       </div>
       <div className={classes.content}>
-        <h3>{props.title}TITLE</h3>
-        <p>{props.description}DESCRIPTION</p>
-      </div>
-      <div>
-        <button className={classes.actions}>TO FAVORITES</button>
+        <h3>{props.title}</h3>
+        <address>{props.address}</address>
+        <p>{props.description}</p>
+        <div className={classes.actions}>
+          <button>TO FAVORITES</button>
+        </div>
       </div>
     </li>
   );
