085b9ec3d40ec7a8c8d79c4c0a8266744103a8d6
mahmoud fouad <mfapolice@gmail.com>
2022-08-17 02:42:32 +0200

    update

--- src/component/meetups/NewMeetupForm.js
+++ src/component/meetups/NewMeetupForm.js
@@ -1,10 +1,23 @@
+import { useRef } from 'react';
+
 import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css';
 
 function NewMeetupForm() {
+  const titleRef = useRef();
+  const imageRef = useRef();
+  const addressRef = useRef();
+  const descriptionRef = useRef();
+  function submitHandler(e) {
+    const enteredTitle = titleRef.current.value;
+    const enteredImage = imageRef.current.value;
+    const enteredAddress = addressRef.current.value;
+    const enteredDescription = descriptionRef.current.value;
+    e.preventDefault();
+  }
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
           <input type="text" required id="title" />
@@ -14,11 +27,11 @@ function NewMeetupForm() {
           <input type="url" required id="image" />
         </div>
         <div className={classes.control}>
-          <label htmlFor="address">Meetup Address</label>
+          <label htmlFor="address"> Address</label>
           <input type="text" required id="address" />
         </div>
         <div className={classes.control}>
-          <label htmlFor="description">Meetup Description</label>
+          <label htmlFor="description"> Description</label>
           <textarea rows="5" required id="description" />
         </div>
         <div>
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,12 @@
+import Card from '../component/ui/Card';
+
 function Profile() {
   return (
     <div>
       <h1>My Profile</h1>
+      <Card>
+        <h1>welcome back, MAHMOUD</h1>
+      </Card>
     </div>
   );
 }
