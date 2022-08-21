2e7cc94670b720ba16daa9a6d500ccda8580e612
mahmoud fouad <mfapolice@gmail.com>
2022-08-22 01:00:53 +0200

    add Nav Links

--- src/component/layout/MainNavbar.js
+++ src/component/layout/MainNavbar.js
@@ -1,8 +1,25 @@
+import { Link } from 'react-router-dom';
+
+import classes from './MainNavbar.module.css';
 function MainNavbar(props) {
   return (
-    <header>
+    <header className={classes.header}>
+      <div className={classes.logo}>Facebook</div>
       <ul>
-        <li>Item</li>
+        <li>
+          <Link to={'/'}>Home</Link>
+        </li>
+        <li>
+          <Link to={'/my-profile'}>Profile</Link>
+        </li>
+        <li>
+          <Link to={'/my-favorites'}>
+            Favorites <span className={classes.badge}>0</span>
+          </Link>
+        </li>
+        <li>
+          <Link to={'/new-post'}>New Post</Link>
+        </li>
       </ul>
       {props.children}
     </header>
