e77249d115aa5ea2caae71112473201185f71b01
mahmoud fouad <mfapolice@gmail.com>
2022-08-21 22:53:30 +0200

    start the project again

--- src/App.js
+++ src/App.js
@@ -1,30 +1,5 @@
-import { Route, Switch } from 'react-router-dom';
-import Layout from './component/layout/Layout';
-
-import AllMeetUpsPage from './pages/AllMeetups';
-import FavoritesPage from './pages/Favorites';
-import NewMeetupPage from './pages/NewMeetUps';
-import Profile from './pages/Profile';
-
 function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <AllMeetUpsPage />
-        </Route>
-        <Route path="/new-meetup">
-          <NewMeetupPage />
-        </Route>
-        <Route path="/favorites">
-          <FavoritesPage />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-      </Switch>
-    </Layout>
-  );
+  return <h1>Hi there</h1>;
 }
 
 export default App;
--- src/pages/Favorites.js
+++
@@ -1,5 +0,0 @@
-function FavoritesPage() {
-  return <h1>Favorites Page</h1>;
-}
-
-export default FavoritesPage;
